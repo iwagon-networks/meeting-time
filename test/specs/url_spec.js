f57684e66f580e45f5c202bd55d537a6832ebd40
Iwa Dev <iwashita_akifumi@yahoo.co.jp>
2016-06-22 17:37:44 +0900

    adjust format

--- test/specs/url_spec.js
+++ test/specs/url_spec.js
@@ -1,5 +1,5 @@
 describe("URL parsing", function() {
-  it("defaults to SanFrancisco and Melbourne", function() {
+  it("defaults to San Francisco and Melbourne", function() {
     expect(Url.get()).toBe('melbourne/sanfrancisco/');
   });
 
